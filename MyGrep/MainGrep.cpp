d2c9b5aec4e702b92602aa9142a9bc346a0df657
Atte Kukkonen <kukkonen16@hotmail.com>
2025-02-28 15:17:41 +0200

    Increment 2

--- MyGrep/MainGrep.cpp
+++ MyGrep/MainGrep.cpp
@@ -1,25 +1,55 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 
-int main() {
+int main(int argc, char* argv[]) {
+	
+	if (argc < 2) {
+		std::string searchSentence;
+		std::string searchString;
 
-	std::string searchSentence;
-	std::string searchString;
+		std::cout << "Give a string from which to search from: " << std::endl;
+		std::getline(std::cin, searchSentence);
+		std::cout << "Give search string: " << std::endl;
+		std::getline(std::cin, searchString);
 
-	std::cout << "Give a string from which to search for: " << std::endl;
-	std::getline(std::cin, searchSentence);
-	std::cout << "Give search string: " << std::endl;
-	std::getline(std::cin, searchString);
+		size_t wordPosition = searchSentence.find(searchString);
 
-	size_t wordPosition = searchSentence.find(searchString);
-
-	if (wordPosition != std::string::npos) {
-		std::cout << "String " << searchString << " found at position " << wordPosition << std::endl;
+		if (wordPosition != std::string::npos) {
+			std::cout << "String " << searchString << " found at position " << wordPosition << std::endl;
+		}
+		else {
+			std::cout << "String " << searchString << " was not found" << std::endl;
+		}
 	}
-	else {
-		std::cout << "String " << searchString << " was not found" << std::endl;
+
+	std::string searchString = argv[1];
+	std::string searchFile = argv[2];
+	std::string line;
+	bool found = false;
+
+	std::ifstream inputFile(searchFile);
+	if (!inputFile) {
+		std::cout << "Unable to open the file" << std::endl;
+		return 1;
+		}
+
+	while (std::getline(inputFile, line)) {
+		if (line.find(searchString) != std::string::npos) {
+			std::cout << line << std::endl;
+			found = true;
+		}	
+	}
+	
+	inputFile.close();
+
+	if (!found) {
+		std::cout << "String not found" << std::endl;
 	}
 
+	
+	
 	return 0;
 
 
